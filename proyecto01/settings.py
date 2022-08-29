d04f11e7bbf756e18466c76a39fec8dec47b1ca5
Mateo.Gomez <gomezmaryncakmateo@gmail.com>
2022-08-28 21:05:56 -0300

    heroku prueba4

--- proyecto01/settings.py
+++ proyecto01/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'proyecto01.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates_inicio')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
